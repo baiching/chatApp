b29de0326d4ffb56baba8ac370831d3672e21027
baiching <bachmarma@gmail.com>
2025-04-27 19:51:19 +0200

    updated chat server which will listen at port 5000

--- src/main/java/com/baiching/server/ChatServer.java
+++ src/main/java/com/baiching/server/ChatServer.java
@@ -1,7 +1,26 @@
 package com.baiching.server;
 
+import com.baiching.client.ClientHandler;
+
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatServer {
+    private static List<ClientHandler> clients = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5000);
+
+        while (true) {
+            Socket socketClient  = serverSocket.accept();
+            System.out.println("Client connected. Waiting for message...");
+
+            ClientHandler clientThread = new ClientHandler(socketClient, clients);
+            clients.add(clientThread);
+            new Thread(clientThread).start();
+
+        }
+    }
 }
