2fc93c37863a659cde6772146f13aa905f28598f
baiching <bachmarma@gmail.com>
2025-05-01 19:36:48 +0200

    added log to ensure server started

--- src/main/java/com/baiching/server/ChatServer.java
+++ src/main/java/com/baiching/server/ChatServer.java
@@ -10,6 +10,7 @@ public class ChatServer {
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(5000);
+        System.out.println("Server started on port 5000. Waiting for clients...");
 
         while (true) {
             Socket socketClient  = serverSocket.accept();
