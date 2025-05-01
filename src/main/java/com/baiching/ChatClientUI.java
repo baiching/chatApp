13f273f8e0f18fd740cfeb2dc124e146ff1dfd57
baiching <bachmarma@gmail.com>
2025-05-01 19:37:53 +0200

    added UI element

---
+++ src/main/java/com/baiching/ChatClientUI.java
@@ -0,0 +1,57 @@
+package com.baiching;
+
+import com.baiching.client.ChatClient;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class ChatClientUI extends JFrame {
+    private JTextArea messageArea;
+    private JTextField textField;
+    private ChatClient client;
+
+    public ChatClientUI() {
+        super("Chat Application");
+        setSize(400, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        messageArea = new JTextArea();
+        messageArea.setEditable(false);
+        add(new JScrollPane(messageArea), BorderLayout.CENTER);
+
+        textField = new JTextField();
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                client.sendMessage(textField.getText());
+                textField.setText("");
+            }
+        });
+
+        add(textField, BorderLayout.SOUTH);
+
+        try {
+            this.client = new ChatClient("127.0.0.1", 5000, this::onMessageReceived);
+            System.out.println("Attempting to connect to server...");
+            client.startClient();
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Error connecting to the server", "Connection error",
+                    JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+    }
+
+    private void onMessageReceived(String message) {
+        SwingUtilities.invokeLater(() -> messageArea.append(message + "\n"));
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new ChatClientUI().setVisible(true);
+        });
+    }
+}
